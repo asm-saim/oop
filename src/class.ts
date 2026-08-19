e72b6f889aaa0f1d78fabcd10aba5b5a1f8b6350
asm-saim <saim777m@gmail.com>
2026-08-19 10:49:17 +0600

    class initiated

---
+++ src/class.ts
@@ -0,0 +1,11 @@
+//class:
+class Student {
+  name: string = "Alice";
+  roll: number = 34;
+  email: string = "alice@gmail.com";
+}
+
+//Now instantiate the class, to get instance(OBJECT)
+
+let alice = new Student(); //instantiate
+console.log(alice);
--- src/object.ts
+++ src/object.ts
@@ -9,6 +9,7 @@ const email2 = "sal@gmail.com";
 //This method is not effective, we have to group related data in a single place.
 //Thats where, object concept comes in.
 
+//concept1:
 const daneal = {
   name: "Daneal",
   age: 23,
@@ -21,4 +22,31 @@ const salma = {
   email: "sal@gmail.com",
 };
 
-//But still the problem is not solved yet, if we forget the email, every person needed to use same procedure, but we can not do it outside the object, cause it will give us the same problem.  
+//But still the problem is not solved yet, if we forget the email, every person needed to use same procedure, but we can not do it outside the object, cause it will give us the same problem.
+
+//concept2:
+const studentInfo = (
+  name: string,
+  age: number,
+  email: string,
+  marks: number,
+) => {
+  const obj = {
+    name,
+    age,
+    email,
+    marks,
+    forgetPass() {
+      //100 line
+    },
+  };
+  return obj;
+};
+
+const Alice = studentInfo("Alice", 24, "alice@gmail.com", 98);
+const JK = studentInfo("JK", 23, "jk@gmail.com", 58);
+
+// easy access: no security.
+Alice.email = "";
+
+console.log(Alice, JK);
