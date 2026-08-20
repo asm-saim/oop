dc8ab2f3b93d6b6271d9d6fa6139819582b4af6f
asm-saim <saim777m@gmail.com>
2026-08-20 09:29:11 +0600

    polymorphism second example implemented

--- src/polymorphism.ts
+++ src/polymorphism.ts
@@ -31,5 +31,51 @@ class Bird extends Animal {
   }
 }
 //making instance:
-const bird= new Bird("Mekao")
-bird.makeSound()
+const bird = new Bird("Mekao");
+bird.makeSound();
+
+//Example2:
+class Area {
+  area(): number {
+    return 0;
+  }
+}
+
+//Circle:
+class Circle extends Area {
+  private _radius: number;
+
+  constructor(radius: number) {
+    super();
+
+    this._radius = radius;
+  }
+
+  area(): number {
+    return 3.1416 * this._radius * this._radius;
+  }
+}
+
+//instance:
+const findCircle = new Circle(5);
+console.log(findCircle.area());
+
+//rectangle:
+class Rectangle extends Area {
+  height: number;
+  width: number;
+
+  constructor(height: number, width: number) {
+    super();
+    this.height = height;
+    this.width = width;
+  }
+
+  area(): number {
+    return this.height * this.width;
+  }
+}
+
+//instance:
+const findRectangle=new Rectangle(5,5);
+console.log(findRectangle.area())
